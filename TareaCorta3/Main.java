5b97269a63f4c5586750ebca7b4147df798e1aef
ignacioroguadamuz-boop <ignacio.roguadamuz@gmail.com>
2026-04-27 14:22:59 -0600

    Implement Pi approximation with user input
    Added user input for iterations and methods to approximate Pi.

--- TareaCorta3/Main.java
+++ TareaCorta3/Main.java
@@ -1,1 +1,21 @@
-...
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingrese cantidad de iteraciones: ");
+        int n = sc.nextInt();
+
+        AproximadorPi aproximador = new AproximadorPi(n);
+
+        aproximador.calcularMonteCarlo();
+        aproximador.calcularLeibniz();
+
+        System.out.println("\n--- RESULTADOS ---");
+        System.out.println(aproximador);
+
+        sc.close();
+    }
+}
